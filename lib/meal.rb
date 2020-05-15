89500cec8add4c4c5f194280799587fb2c89c532
Paul Beckwith <303dog@gmail.com>
2020-05-15 22:36:39 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,13 @@
 class Meal
 
+  attr_accessor :waiter, :customer, :totla, :authenticate_or_request_with_http_digest
+  @@all = []
+  
+  def initialize(waiter, custmoer, total, tip=0)
+    @waiter =waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
 end
