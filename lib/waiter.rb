49b899b301347bc5a8f9855e1dc71577029f77c9
Paul Beckwith <303dog@gmail.com>
2020-05-15 22:30:52 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,13 @@
 class Waiter
-
+  
+  attr_accessor :name, :yrs_experience
+  
+  @@all = []
+  
+  def initiaiize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
+  end
+  
 end
